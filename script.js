56dfa9fb3ef577663e2952c9dbb823b5e6d0be80
Tim <71687982+DjTim0n@users.noreply.github.com>
2023-06-21 18:05:55 +0600

    +! section and a liltle fix

---
+++ script.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const names = ["whale", "squid", "turtle", "coral", "starfish"];
+
+const NamesList = () => (
+  <div>
+    <ul>
+      {names.map((name) => (
+        <li key={name}> {name} </li>
+      ))}
+    </ul>
+  </div>
+);
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<NamesList />, rootElement);
