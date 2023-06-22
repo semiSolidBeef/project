5366705e4545798391393b343b4e18f44c2fbef6
Tim <71687982+DjTim0n@users.noreply.github.com>
2023-06-22 20:52:32 +0600

    bugfix a little

--- script.js
+++ script.js
@@ -28,22 +28,30 @@ const blocks = data.map((item) => {
   const block = document.createElement("div");
   block.className = "block";
 
+	
   const image = document.createElement("img");
   image.className = "image";
   image.textContent = item.image;
   image.src = item.image;
-
+	
   const title = document.createElement("h2");
   title.className = "title";
   title.textContent = item.title;
-
+	
   const description = document.createElement("p");
   description.className = "description";
   description.textContent = item.description;
+	
+	const link = document.createElement("a");
+	link.className = "link";
+  link.textContent = item.link;
+	link.href = '#';
 
   block.appendChild(image);
   block.appendChild(title);
   block.appendChild(description);
+	block.appendChild(link);
+
 
   return block;
 });
