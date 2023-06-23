9e6782a0e7641eb89468c1c4b03213fd563f69ca
Tim <71687982+DjTim0n@users.noreply.github.com>
2023-06-23 23:36:41 +0600

    ae sasniy(ya-ebal-menya-sosali)

--- script.js
+++ script.js
@@ -59,3 +59,24 @@ const blocks = data.map((item) => {
 blocks.forEach((block) => {
   parentElement.appendChild(block);
 });
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
